e86358c8aa76ed098ba5ea5d492473f209fa4cbd
Antonin Lambilliotte <antonin.lambilliotte@ens-lyon.fr>
2016-12-02 11:34:39 +0100

    Analyse

--- final.R
+++ final.R
@@ -3,14 +3,21 @@ teneur=data$teneur
 testeur=data$testeur
 origine=data$origine
 
-#GRAPHIQUES 
-
 #Influence de chaque variable séparée
-ggplot(data,aes(x=as.numeric(testeur), y=teneur))+geom_point()
-ggplot(data,aes(x=as.numeric(origine), y=teneur))+geom_point()
 
-#Influence des deux variables simultanées avec un interaction plot classique
-interaction.plot(testeur,origine,teneur)
+#influence de l'origine
+ggplot(data,aes(x=origine, y=teneur))+geom_point()
+lm2=lm(teneur~origine)
+summary(lm2)
+plot(lm2)
+
+#influence du testeur
+ggplot(data,aes(x=testeur, y=teneur))+geom_point()
+lm1=lm(teneur~testeur)
+summary(lm1)
+plot(lm1)
+
+#Influence des deux variables simultanées
 
 #Reproduction d'un interaction plot avec ggplot
 newdata=as.data.frame.table(with(data, tapply(teneur, list(testeur, origine), mean)),dnn=c("newtesteur",'neworigine','newteneur'))
@@ -19,30 +26,7 @@ newtesteur=newdata$Var1
 neworigine=newdata$Var2
 ggplot(newdata,aes(x=newtesteur,y=newteneur,group=neworigine,col=neworigine))+geom_line()+ylab("Teneur moyenne")+xlab("Testeur")+labs(colour = "Origine")
 
-
-par(mfrow=c(2,2))
-#MODELES LINEAIRES
-
-#influence du testeur
-lm1=lm(teneur~testeur)
-anova(lm1)
-summary(lm1)
-plot(lm1)
-
-#influence de l'origine
-lm2=lm(teneur~origine)
-anova(lm2)
-summary(lm2)
-plot(lm2)
-
 #influence des deux avec interaction
-lm3=lm(teneur~ftesteur*forigine)
-anova(lm3)
+lm3=lm(teneur~testeur*origine)
 summary(lm3)
 plot(lm3)
-
-#influence des deux sans interaction
-lm4=lm(teneur~ftesteur+forigine)
-anova(lm4)
-summary(lm4)
-plot(lm4)
